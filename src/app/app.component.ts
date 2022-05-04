405ea300fb7fd144010f72d57a4543cd7147c986
Gery <gerytsv@gmail.com>
2022-05-04 21:13:42 +0300

    Google Books initial version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'google-books';
+  API_KEY = 'AIzaSyCk6teXMybrdfSnfqXBPpnh_8lJAk-Wwgk';
+  input: string = '';
+  books: any[] = [];
+
+  onSubmit(form: NgForm) {
+    fetch(
+      `https://www.googleapis.com/books/v1/volumes?q=${this.input}` //&key=${this.API_KEY}
+    )
+      .then((response) => response.json())
+      .then((result) => {
+        this.books = result.items;
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
+import { BooksModule } from './books/books.module';
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    CommonModule,
+    BrowserAnimationsModule,
+    BooksModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
